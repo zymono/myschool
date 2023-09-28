869719e9cf109506a73d6f5923e14ffc23d413b9
Lava <63625087+cosmixcom@users.noreply.github.com>
2023-09-28 16:29:54 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
-        '/'
+        '/LICENSE'
       ]);
     })
   );
