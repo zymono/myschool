d184fea985a0b9ac83b3f9bb81f4050e002ce2df
Lava <63625087+cosmixcom@users.noreply.github.com>
2023-09-28 16:29:29 -0700

    Update check.js

--- report/check.js
+++ report/check.js
@@ -1,7 +1,7 @@
 // import { checkIfUser } from './backend/account.js'
 // checkIfUser()
   if (window.localStorage.getItem('uid') == null) {
-    window.location.href = '/flow/signup/?b=' + window.location
+    window.location.href = '/flows/signup/?b=' + window.location
   } else {
         console.log('%cBe Careful Do Not Paste Anything Here If You Don\'t Know What You Are Doing, It Could Give Hackers Access To Your Account!', 'color: red; font-size: 40px; font-weight: bold;');
     console.log('%cIf you do know what you are doing you should contribute to our development at https://zymono.com/contribute!', 'color: green; font-size: 20px;');
